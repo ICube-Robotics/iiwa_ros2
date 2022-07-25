828439574bc54e8821744893f734ce51c09bafea
Maciej Bednarczyk <macbednarczyk@gmail.com>
2022-07-25 13:07:43 +0200

    set up gazebo envar

--- .docker/ros_entrypoint.sh
+++ .docker/ros_entrypoint.sh
@@ -4,4 +4,6 @@ set -e
 # setup ros environment
 source "/opt/ros/$ROS_DISTRO/setup.bash"
 source "/ros2_dev/iiwa_ros2/install/setup.bash"
+source "/usr/share/gazebo/setup.sh"
+export GAZEBO_MODEL_PATH=$GAZEBO_MODEL_PATH:/ros2_dev/iiwa_ros2/
 exec "$@"
